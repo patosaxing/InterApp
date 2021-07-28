226c416d04ef9bf064aaaced658fc0039baaa529
Patrick Osaje <patrickosaje@gmail.com>
2021-07-28 13:41:17 -0600

    secondandahalf

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const app = express()
 
 const port = 3000
 
+app.get('/', (req, res) =>
+{
+    res.render()
+})
+
 app.listen(port, () =>
 {
     console.log(`Application listening at http://localhost:${port}`)
