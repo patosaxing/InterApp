85c208c821fc64380b36526ecbc2af08b8002cf8
Patrick Osaje <patrickosaje@gmail.com>
2021-08-20 14:57:10 -0600

    working

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const authControl = require("../Controllers/authControl")
 const {protect, admin} = require("../Middleware/auth")
 
-router.post("/users/register", authControl.register)
+router.post("/register", authControl.register)
 
 // router.get("/", async(req,res) =>
 // {
