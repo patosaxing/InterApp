d7789bf99e5e34842c3b49e0b8967cede1a21cad
Patrick Osaje <patrickosaje@gmail.com>
2021-08-30 13:50:55 -0600

    change

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -5,6 +5,7 @@ const authControl = require("../Controllers/authControl")
 const {protect, admin} = require("../Middleware/auth")
 
 router.post("/register", authControl.register)
+router.post("/login", authControl.login)
 
 // router.get("/", async(req,res) =>
 // {
