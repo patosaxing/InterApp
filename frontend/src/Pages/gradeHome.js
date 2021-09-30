31d5d63d54a304a2dcb02378c21fe158ac6266a2
Patrick <patrickosaje@gmail.com>
2021-09-30 03:25:47 -0600

    front3

--- frontend/src/Pages/gradeHome.js
+++ frontend/src/Pages/gradeHome.js
@@ -1,12 +1,46 @@
 import React from 'react'
-//import './CSS/home.css'
+import '../Pages/CSS/home.css'
 //import Navbar from '../Components/Navbar'
 
 const gradeHome = () =>
 {
     return (
         <div>
-            
+            <form action = "" className = "form">
+                <input type = "text" placeholder = "Email" />
+                <br />
+
+                <input type = "password" placeholder = "Password" />
+                <br />
+
+                <button type = "submit">
+                    Log In
+                </button>
+                <br />
+
+                <button className = "btn2">
+                    Create Account
+                </button>
+
+                <div className = "line"></div>
+                <br />
+
+                <a href = "" className = "f-pws">
+                    Forget Password
+                </a>
+                <br />
+
+            </form>
+
+            <div className = "inter">
+                <h1 className = "header">
+                    InterApp
+                </h1> 
+
+            <p className = "paragraph1">
+                    Find your path
+            </p>
+            </div>
         </div>
     )
 }
