7e6924e1cc9748c2aa28b48d6883d16973eaf0cd
Patrick <patrickosaje@gmail.com>
2021-10-19 13:28:06 -0600

    inter

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,11 +1,17 @@
 //Import Statements
 const express = require("express")
+const { check, validationResult } = require("express-validator")
 const router = express.Router()
 const authControl = require("../Controllers/authControl")
 const {protect, admin} = require("../Middleware/auth")
 
 
-router.post("/register", authControl.register)
+router.post("/register", [
+        check("firstName", "First Name is Required").not().isEmpty(),
+        check("lastName", "Last Name is Required").not().isEmpty(),
+        check("email1", "Please include a valid email").isEmail(),
+        check("userName", "Please enter your User Name").not().isEmpty()
+], authControl.register)
 router.post("/login", authControl.login)
 
 // router.get("/", async(req,res) =>
